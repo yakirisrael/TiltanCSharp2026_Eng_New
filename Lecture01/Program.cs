ba3df0dcfbcbabcc03360ba321cd554d297693c8
Your Name <yakir.israel1@gmail.com>
2025-12-23 14:54:04 +0200

    first time uploading to Git

---
+++ Lecture01/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string message = "Please enter your age:";
+Console.WriteLine(message);
+string ageStr = Console.ReadLine();
+int age = int.Parse(ageStr);
+
+/*
+Console.WriteLine("your age is " + age);
+Console.WriteLine("enter your age again");
+*/
+string ageStr2 = Console.ReadLine();
+
---
+++ Lecture02/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+int score = 100;
+
+score = 20;
+float age = 22;
+string ageStr = "22.3";
+
+int x = -20;
+int y = 15;
+
+
+bool FirstBool = true;
+bool SecondBool = false;
+
+if (SecondBool)
+    Debug.WriteLine("Hello Debugger");
+    Debug.WriteLine("Hello after Debugger");
+
